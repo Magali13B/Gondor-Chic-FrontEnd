c4df62476a7accaed5fb39e620e2a1a1baa9a1c5
Manitra <mrakotoarisoa@eda-i.com>
2025-06-13 11:39:59 +0300

    initialize login component

---
+++ src/app/components/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: '../templates/login.html',
+  styleUrl: '../styles/login.css'
+})
+export class Login {
+
+}
