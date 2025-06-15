b49ba15b77a2002af895c2e66318f466726173b1
MaheryTiana <maherytianaandria28@gmail.com>
2025-06-15 20:54:50 +0300

    addToCart page

---
+++ src/app/add-to-cart/add-to-cart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-cart',
+  imports: [],
+  templateUrl: '../templates/add-to-cart.html',
+  styleUrl: '../styles/add-to-cart.css'
+})
+export class AddToCart {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {Login} from './components/login';
+import {AddToCart} from './add-to-cart/add-to-cart';
 
 export const routes: Routes = [
   {
-    path : '', component : Login
+    path : '', component : Login  
+  },
+  {
+    path : 'addToCart', component : AddToCart 
   }
 ];
