dd32e123cb38dc8ce9eeece4a3046f801458e5b3
Manitra <shibamiyuki07@yahoo.com>
2025-06-16 12:29:43 +0300

    create product page

---
+++ src/app/components/createproduct.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createproduct',
+  imports: [],
+  templateUrl: '../templates/createproduct.html',
+  styleUrl: '../styles/createproduct.css'
+})
+export class Createproduct {
+
+}
